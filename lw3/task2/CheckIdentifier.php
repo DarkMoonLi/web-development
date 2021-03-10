f418d59d12dc1f94faad4a9c95627510919fd03b
Maxim <DarkMoonLi>
2021-03-10 12:47:27 +0000

    message

---
+++ lw3/task2/CheckIdentifier.php
@@ -0,0 +1,19 @@
+<?php
+function getGETParameter(string $name):?string
+{
+    return isset($_GET[$name])?(string)$_GET[$name]: null;
+}
+header("Content-Type: text/plain");
+$Identifier = getGETParameter('identifier');
+if (ctype_alnum($Identifier))
+    {
+        $Logic = true;
+    }
+if ($Logic==true)
+    {
+        echo "Yes";
+    }
+else
+    {
+        echo "No";
+    }
